228f738337f36d3c2792af7d0c4f8cc11bd005d8
ramayaser66 <ramayaser66@gmail.com>
2020-12-29 02:40:46 +0200

    new css changes

--- javascript.js
+++ javascript.js
@@ -1,69 +1,86 @@
 'use strict';
-alert("welcome to my page")
-var welcomeNote = confirm("do you feel like playing a game ?")
+alert("welcome to my page");
+var welcomeNote = confirm(" Do you feel like playing a game? ");
+console.log(welcomeNote);
 
-alert("let's go!")
-var q1 = prompt(" what's your name? ")
-console.log(q1);
-alert(" Nice to meet you " + q1);
+if (welcomeNote === true) {
+    alert (" let's go! ");
+    
+var q1 = prompt (" what is your name? ");
+//console.log(q1);  
+alert (" Nice to meet you " + q1 );  
 
-var q2 = prompt(" do you think we are of the same age?")
-console.log(q2);
-//switch (q2) {
-
-// case 'yes' : 
-//alert (' you are right!');
-//break;
-
-// case 'no' :
-//   alert ('Guess again!');
-// break; 
-//}
-
-if (q2.toLocaleLowerCase() === 'yes') {
-    alert('you are right!')
-} else if (q2.toLocaleUpperCase() === 'no') {
-    alert('Guess again!')
+var q2 = prompt(" Do you think we're of the same age? ");
+//console.log(q2);
+if (q2 === 'yes'){
+    alert (' that is correct! '); 
+} else {
+    alert (" Guess again! "); 
 }
 
 
-var q3 = prompt("how old do you think i am?")
-
-console.log(q3);
-
-if (q3 === '22') {
-    alert(' right again :) '); 
-}
-else if (q3 > '22') {
-    alert('try lower :( ');
-
-} else if (q3 < '22') {
-    alert('try higher :( ');
-}
-
-var q4 = prompt('do you think i like pets')
-console.log(q4);
-if (q4.toLocaleLowerCase === 'yes') {
-    alert(" that's correct ");
-
-} else if (q4.toLocaleUpperCase === 'no') {
-    alert(" that's wrong ");
+var q3 = prompt (" How old do you think I am ?"); 
+//console.log(q3); 
+if (q3 == '22'){
+    alert ("Right again");
+} else if (q3 > '22'){
+    alert (" try younger :( ");
+} else if (q3 < '22'){
+    alert ("try older :) ");
 }
 
 
-var q5 = prompt('do you think i like movies')
-switch (q5) {
+var q4 = prompt ("would you say I am an animle person ?"); 
+//console.log(q4);
+if (q4.toLocaleLowerCase() === 'yes'){
+    alert ("indeed I am my friend!  "); 
+    var animale = prompt ("Who do you think I like more Cats or Dogs!");
+    if (animale === 'dogs'){
+        alert (" you're on a roll :)");
+    } else if (animale === 'cats'){
+        alert (" I like them, bit not as much ")
+    }
+} else if (q4.toLocaleLowerCase() === 'no'){
+    alert (" sadly you are mistaken ");
+}
+
+
+var q5 = prompt (" DO think i like sports?  ");
+//console.log(q5);
+switch (q5){
     case 'yes':
-        alert('you are right, i like watching movies alot');
-        break;
-
+        alert("you are right, I love sports")
+        break; 
+    
     case 'no':
-        alert('think again!');
-        break;
+        alert(" guess again :( ");
+        break; 
 }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} else if (welcomeNote === false){
+    alert (" goodbey!");}
 
