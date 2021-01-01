ad8391a0b81e380d34fde1a8a739a9073a40f8ad
ramayaser66 <ramayaser66@gmail.com>
2021-01-02 01:29:46 +0200

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 201-webpage
+
+## the driver: khaled ziadeh
+## the navigator: rama yaser
+___________________________________________
+
+- we fixed some codes
+- we added functions
--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 'use strict';
+
 alert("welcome to my page");
 var welcomeNote = confirm(" Do you feel like playing a game? ");
 console.log(welcomeNote);
@@ -7,194 +8,179 @@ var counterOfCorrectAnswers = 0;
 if (welcomeNote === true) {
     alert(" let's go! ");
 
-    var q1 = prompt(" what is your name? ");
-    console.log(q1);
-    alert(" Nice to meet you " + q1 + " hope you enjoy this site ");
+    firstFunction();
+    secondFunaction();
+    thirdFunction();
+    forthFunction();
+    fifthFunction();
+    sixthFunction();
+    saventhFunction();
+    scoreFunction();
 
+    function firstFunction() {
+        var q1 = prompt(" what is your name? ");
+        console.log(q1);
+        alert(" Nice to meet you " + q1 + " hope you enjoy this site ");
+    }
 
-    var q2 = prompt(" Do you think we're of the same age? ");
-    console.log(q2);
-    if (q2 === 'yes') {
-        alert(' shall we check ? ');
-        counterOfCorrectAnswers++;
-
-        var userAge = prompt("how old are you?");
-        console.log(userAge);
-        if (userAge === '22') {
-            alert(" we are of the same age! ");
+    function secondFunaction() {
+        var q2 = prompt(" Do you think we're of the same age? ");
+        console.log(q2);
+        if (q2 === 'yes') {
+            alert(' shall we check ? ');
             counterOfCorrectAnswers++;
-        } else if (userAge > '22') {
-            alert(" I guess Iam younger :( ");
-        } else if (userAge < '22') {
-            alert(" I am older than you :) ");
-        }
 
-    } else {
-        alert(" maybe we are ");
-        var age = prompt("how old are you");
-        if (age === '22') {
-            alert(" we are of the same age! ");
-        } else if (age > '22') {
-            alert(" I guess Iam younger :( ");
-        } else if (age < '22') {
-            alert(" I am older than you :) ");
-        }
-
-    }
-
-
-    // var q10 = prompt(" How old do you think I am ?");
-    //console.log(q3); 
-    // if (q10 == '22') {
-    //  alert("Right again");
-    //  } else if (q10 > '22') {
-    // alert(" try younger :( ");
-    //  } else if (q10 < '22') {
-    // alert("try older :) ");
-    // }
-
-    var q3 = prompt('do you think I am doing a good job so far');
-    console.log(q3);
-    if (q3.toLowerCase() === 'yse') {
-        alert("thank you, you're so nice!");
-        console.log(q3);
-
-        counterOfCorrectAnswers++;
-    } else if (q3.toLowerCase() === 'no') {
-        alert('thank you for being honest, i will try to do my best');
-    }
-
-
-    var q4 = prompt("do you think I like animals ?");
-    //console.log(q4);
-    if (q4.toLocaleLowerCase() === 'yes') {
-        alert("indeed I do my friend!  ");
-        counterOfCorrectAnswers++;
-        var animale = prompt("what do you think I like more Cats or Dogs!");
-        if (animale === 'dogs') {
-            alert(" you're on a roll :)");
-            counterOfCorrectAnswers++;
-        } else if (animale === 'cats') {
-            alert(" I like them, bit not as much ");
-        }
-    } else if (q4.toLocaleLowerCase() === 'no') {
-        alert(" sadly you are mistaken ");
-    }
-
-
-    var q5 = prompt(" DO think i like sports?  ");
-    //console.log(q5);
-    switch (q5) {
-        case 'yes':
-            alert("you are right, I love sports");
-            counterOfCorrectAnswers++;
-            break;
-
-        case 'no':
-            alert(" guess again :( ");
-            break;
-    }
-
-
-
-
-
-
-    //var  note = prompt (" guess my favorite number, you have 4 guesses");
-    //  console.log(note);
-    alert(' the next game should be fun, try to guess!');
-    alert(" you have 4 guesses");
-    for (var i = 0; ((i < 4) && (note !== '7')); i++) {
-
-
-        var note = prompt(" guess my favorite number");
-        console.log(note);
-        //alert(" you have 4 guesses");
-
-        alert("I'll give you a hint, it's lower than 10 and some say it's a lucky number! ");
-
-
-        var int = parseInt(note);
-        if (int == 7) {
-            alert("that's correct");
-            counterOfCorrectAnswers++;
-        } if (int < 7) {
-            alert(" try higher!");
-
-        } else if (int > 7) {
-            alert(" try lower! ");
-        } else {
-
-            break;
-        }
-
-
-
-
-
-    }
-    alert(" the correct answer is 7 ");
-
-    // var movieType = ['comedy', 'horror', 'superheros', "crime"];
-    //console.log(movieType [1]); 
-    //var q = movieType.length;
-    // for ( var i =0; ((i < 6) && (i !== movieType.length)); i++){
-    //  var q7 = prompt(" what type of movies do you think I prefer? ");
-    // console.log (q7); 
-    //console.log(movieType[i]);
-    //alert ("you have 6 guesses");
-
-
-    //}
-    // var t = 0;
-    //var check = false;
-    //do{
-
-    // var q7 = prompt(" what type of movies do you think I prefer? ");
-    //  console.log(j);
-    //var j=0;
-    //t++;
-
-
-    //} while (movieType.length < 6  &&  q7 !== movieType.length){
-    //  alert (" again! ")
-    //}
-
-    var movieType = ['comedy', 'horror', 'superheros', "crime", 'foreign', 'anime'];
-
-    for (var i = 0; i < 5; i++) {
-        var q7 = prompt(" what type of movies do you think I prefer? ");
-        // console.log(movieType[i]);
-       // breack;
-
-        for (var index = 0; index < movieType.length; index++) {
-
-
-
-            if (q7 === movieType[index]) {
-                alert(" correct!, that's one of them");
+            var userAge = prompt("how old are you?");
+            var age1 = parseInt(userAge);
+            if (age1 === 22) {
+                alert(" we are of the same age! ");
                 counterOfCorrectAnswers++;
+            } else if (age1 > 22) {
+                alert(" I guess Iam younger :( ");
+            } else if (age1 < 22) {
+                alert(" I am older than you :) ");
+            }
+
+        } else {
+            alert(" maybe we are ");
+            var age = prompt("how old are you");
+            var age2 = parseInt(age);
+            if (age2 === 22) {
+                alert(" we are of the same age! ");
+            } else if (age2 > 22) {
+                alert(" I guess Iam younger :( ");
+            } else if (age2 < 22) {
+                alert(" I am older than you :) ");
+            }
+
+        }
+    }
+
+
+    function thirdFunction() {
+        var q3 = prompt('do you think I am doing a good job so far');
+        console.log(q3);
+        if (q3.toLowerCase() === 'yes') {
+            alert("thank you, you're so nice!");
+            console.log(q3);
+
+            counterOfCorrectAnswers++;
+        } else if (q3.toLowerCase() === 'no') {
+            alert('thank you for being honest, i will try to do my best');
+        }
+    }
+
+    function forthFunction() {
+        var q4 = prompt("do you think I like animals ?");
+
+        if (q4.toLocaleLowerCase() === 'yes') {
+            alert("indeed I do my friend!  ");
+            counterOfCorrectAnswers++;
+            var animale = prompt("what do you think I like more Cats or Dogs!");
+            if (animale.toLowerCase() === 'dogs' || animale.toLowerCase() === "dog") {
+                alert(" you're on a roll :)");
+                counterOfCorrectAnswers++;
+            } else if (animale.toLowerCase() === 'cats' || animale.toLowerCase() === "cat") {
+                alert(" I like them, bit not as much ");
+            }
+        } else if (q4.toLocaleLowerCase() === 'no') {
+            alert(" sadly you are mistaken ");
+        }
+    }
+
+    function fifthFunction() {
+        var q5 = prompt(" DO think i like sports?  ");
+
+        switch (q5) {
+            case 'yes':
+                alert("you are right, I love sports");
+                counterOfCorrectAnswers++;
+                break;
+
+            case 'no':
+                alert(" guess again :( ");
+                break;
+        }
+
+    }
+
+
+
+
+    function sixthFunction() {
+        alert(' the next game should be fun, try to guess!');
+        alert(" you have 4 guesses");
+
+        for (var i = 0; ((i < 4) && (note !== '7')); i++) {
+
+
+            var note = prompt(" guess my favorite number");
+            console.log(note);
+
+
+
+
+            var int = parseInt(note);
+            if (int == 7) {
+                alert("that's correct");
+                counterOfCorrectAnswers++;
+                break;
+            } if (int < 7) {
+                alert(" try higher!");
+                alert("I'll give you a hint, it's lower than 10 and some say it's a lucky number! ");
+
+            } else if (int > 7) {
+                alert(" try lower! ");
+                alert("I'll give you a hint, it's lower than 10 and some say it's a lucky number! ");
+            } else {
+                alert(" the correct answer is 7 ");
                 break;
             }
 
-         else {
 
-            alert(" care to try again? ");
+
 
 
         }
-        //breack;
-    } 
-   // break; 
-    }
-    alert(" the correct answers: comedy, horror, superheros, crime, foreign, anime ");
-
-    var score = prompt(" wanna know your score ");
-    if (score = 'yes') {
-        alert(" your score is " + counterOfCorrectAnswers++ + 'out of 7');
-
     }
 
+    function scoreFunction() {
+        var score = prompt(" wanna know your score ");
+        if (score = 'yes') {
+            alert(" your score is " + counterOfCorrectAnswers++ + 'out of 7');
+
+        }
+    }
+
+    function saventhFunction() {
+        var movieType = ['comedy', 'horror', 'superheros', "crime", 'foreign', 'anime'];
+        for (var k = 0; k < movieType.length; k++) {
+
+            var userGuess = prompt("what type of movies do you think I prefer? ");
+            console.log('test' + userGuess)
+            for (var j = 0; j < movieType.length; j++) {
+
+                if (userGuess == movieType[j]) {
+                    alert("yes that one of thim");
+
+                    k = 6;
+                    j = 6;
+                    console.log("i'm here in if ");
+                }
+
+
+
+            }
+
+            if (k == 6) {
+
+                alert("these are the correct answers " + movieType.toString());
+                break;
+            }
+            else {
+                alert("try again");
+            }
+        }
 
 
 
@@ -203,10 +189,10 @@ if (welcomeNote === true) {
 
 
 
+    }
+}
 
-
-
-} else if (welcomeNote === false) {
+else if (welcomeNote === false) {
     alert(" goodbey!");
 }
 
